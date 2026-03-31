42019a2fc131a607a5935374059acdcf7f339e51
Moita2025 <2380758219@qq.com>
2026-03-31 17:48:53 +1300

    feat: Create `katex-init.js`

---
+++ custom/vendor-init/katex-init.js
@@ -0,0 +1,15 @@
+function katexRender(
+    ele = document.body,
+    settings = {
+        delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false }
+        ]
+    }
+) {
+    renderMathInElement(ele, settings);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    katexRender();
+});
